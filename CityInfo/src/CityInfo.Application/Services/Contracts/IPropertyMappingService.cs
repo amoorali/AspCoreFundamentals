81f568e355f407c8d231d6e7d3010a085c9dc699
AmirAli Fallahi <amirfallahi222@gmail.com>
2026-01-06 23:45:59 +0330

    added property validation check for better debugging

--- CityInfo/src/CityInfo.Application/Services/Contracts/IPropertyMappingService.cs
+++ CityInfo/src/CityInfo.Application/Services/Contracts/IPropertyMappingService.cs
@@ -5,5 +5,6 @@ namespace CityInfo.Application.Services.Contracts
     public interface IPropertyMappingService
     {
         Dictionary<string, PropertyMappingValue> GetPropertyMapping<TSource, TDestination>();
+        bool ValidMappingExistsFor<TSource, TDestination>(string fields);
     }
 }
