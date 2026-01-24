be31ac3cbd303daf39fe79535fd409f91afe86a1
amoorali <amirfallahi222@gmail.com>
2026-01-24 13:54:54 +0330

    added a resource parameters class for points of interest

---
+++ CityInfo/src/CityInfo.Application/Common/ResourceParameters/PointsOfInterestResourceParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace CityInfo.Application.Common.ResourceParameters
+{
+    public class PointsOfInterestResourceParameteres
+    {
+        #region [ Fields ]
+        const int maxPageSize = 20;
+        public string? Name { get; set; }
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+
+        public string OrderBy { get; set; } = "Name";
+        public string? Fields { get; set; }
+        #endregion
+    }
+}
